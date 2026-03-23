e5a8d77455286686ebcf50abdd4004ed18d30675
alladinalshakir2026-ops <alladinalshakir2026@gmail.com>
2026-03-24 01:07:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,37 @@
 import telebot
 
 TOKEN = "8769822799:AAHV6H1BAfh-Re59VBHUsIvIRrDQejY9SsE"
+ADMIN_ID = 7781703083
 
 bot = telebot.TeleBot(TOKEN)
 
+user_data = {}
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Assalomu alaykum! Bot ishlayapti ✅")
+    bot.send_message(message.chat.id, "Assalomu alaykum!\nIsmingizni kiriting:")
+    user_data[message.chat.id] = {}
+
+@bot.message_handler(func=lambda message: True)
+def handle(message):
+    chat_id = message.chat.id
+
+    if 'name' not in user_data[chat_id]:
+        user_data[chat_id]['name'] = message.text
+        bot.send_message(chat_id, "Telefon raqamingizni kiriting:")
+    
+    elif 'phone' not in user_data[chat_id]:
+        user_data[chat_id]['phone'] = message.text
+        bot.send_message(chat_id, "Muammoingizni yozing:")
+    
+    elif 'problem' not in user_data[chat_id]:
+        user_data[chat_id]['problem'] = message.text
+
+        text = f"📩 Yangi murojaat:\n\n👤 Ism: {user_data[chat_id]['name']}\n📞 Tel: {user_data[chat_id]['phone']}\n📝 Muammo: {user_data[chat_id]['problem']}"
+
+        bot.send_message(ADMIN_ID, text)
+        bot.send_message(chat_id, "✅ Murojaatingiz yuborildi!")
+
+        user_data.pop(chat_id)
 
 bot.polling()
